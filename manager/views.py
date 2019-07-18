28128f7d3495c9dfdf376c1f1da962b0fc255a63
Tuitoek <sjtoek@gmail.com>
2019-07-18 16:46:07 +0300

    Made the clock to keep updating itself

--- manager/views.py
+++ manager/views.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import datetime as dt
+import time
 import turtle
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.setup(width=600,height=600)
 wn.title("Clock")
+wn.tracer(0)
 
 pen = turtle.Turtle()
 pen.hideturtle()
 pen.speed(0)
 pen.pensize(3)
 
-def drawclock(pen):
+def drawclock(h,m,s,pen):
     #Draw clock face
     pen.up()
     pen.goto(0,210)
@@ -34,26 +36,48 @@ def drawclock(pen):
         pen.goto(0,0)
         pen.rt(30)
 
+    #Draw the hands
+    #Draw the hour hands
+    pen.penup()
+    pen.goto(0,0)
+    pen.color("white")
+    pen.setheading(90)
+    angle = (h/12) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(100)
 
+    #Draw the minute hands
+    pen.penup()
+    pen.goto(0,0)
+    pen.color("white")
+    pen.setheading(90)
+    angle = (m/60) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(180)
 
-drawclock(pen)
+    #Draw the second hands
+    pen.penup()
+    pen.goto(0,0)
+    pen.color("white")
+    pen.setheading(90)
+    angle = (s/60) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(60)
 
+while True:
+    h = int(time.strftime("%I"))
+    m = int(time.strftime("%M"))
+    s = int(time.strftime("%I"))
 
+    drawclock(h,m,s,pen)
+    wn.update()
 
+    time.sleep(1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pen.clear()
 
 wn.mainloop()
 

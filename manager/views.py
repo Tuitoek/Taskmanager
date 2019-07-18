c965867028240dd861241b4efaacd6bea2959807
Tuitoek <sjtoek@gmail.com>
2019-07-18 16:19:10 +0300

    Draw the time intervals

--- manager/views.py
+++ manager/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 import turtle
 wn = turtle.Screen()
 wn.bgcolor("black")
-wn.setup(width=400,height=400)
+wn.setup(width=600,height=600)
 wn.title("Clock")
 
 pen = turtle.Turtle()
@@ -12,6 +12,31 @@ pen.hideturtle()
 pen.speed(0)
 pen.pensize(3)
 
+def drawclock(pen):
+    #Draw clock face
+    pen.up()
+    pen.goto(0,210)
+    pen.setheading(180)
+    pen.color("blue")
+    pen.pendown()
+    pen.circle(210)
+
+    #Draw the lines for the hours
+    pen.penup()
+    pen.goto(0,0)
+    pen.setheading(90)
+
+    for i in range(12):
+        pen.fd(190)
+        pen.pendown()
+        pen.fd(20)
+        pen.penup()
+        pen.goto(0,0)
+        pen.rt(30)
+
+
+
+drawclock(pen)
 
 
 
